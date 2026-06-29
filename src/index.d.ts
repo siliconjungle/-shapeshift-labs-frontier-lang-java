cc278ec33a071281ee55cade77263be6f5cb17ed
James Addison <siliconjungle.io@gmail.com>
2026-06-30 09:10:02 +1000

    Release 0.1.18 compiler refresh

--- src/index.d.ts
+++ src/index.d.ts
@@ -16,13 +16,13 @@ export declare const JavaSupportedExtensions: readonly string[];
 
 export interface JavaLanguagePackageMetadata {
   readonly packageName: '@shapeshift-labs/frontier-lang-java';
-  readonly version: '0.1.13';
+  readonly version: '0.1.18';
   readonly sourceLanguage: 'java';
   readonly parser: 'javac';
   readonly parserAstFormat: 'java-ast';
   readonly supportedExtensions: readonly string[];
   readonly compilerPackage: '@shapeshift-labs/frontier-lang-compiler';
-  readonly compilerVersion: '0.2.71';
+  readonly compilerVersion: '0.2.244';
 }
 
 export declare const JavaLanguagePackage: JavaLanguagePackageMetadata;
--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ export const JavaSupportedExtensions = Object.freeze(['.java']);
 
 export const JavaLanguagePackage = Object.freeze({
   packageName: '@shapeshift-labs/frontier-lang-java',
-  version: '0.1.13',
+  version: '0.1.18',
   sourceLanguage: JavaSourceLanguage,
   parser: JavaParser,
   parserAstFormat: JavaParserAstFormat,
   supportedExtensions: JavaSupportedExtensions,
   compilerPackage: '@shapeshift-labs/frontier-lang-compiler',
-  compilerVersion: '0.2.71'
+  compilerVersion: '0.2.244'
 });
 
 export const JavaCapabilityLanguageProfiles = Object.freeze(
